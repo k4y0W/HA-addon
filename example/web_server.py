db5fd05bd3f64b748ea1dbf78d5a2991c744c4cf
k4y0W <wojenskipawel945@gmail.com>
2025-11-26 15:59:35 +0100

    1.2.75

--- example/web_server.py
+++ example/web_server.py
@@ -225,8 +225,27 @@ HTML_PAGE = """
 </div>
 
 <div class="modal fade" id="installModal" tabindex="-1">
-  <div class="modal-dialog"><div class="modal-content"><div class="modal-header bg-light"><h5 class="modal-title">Instalacja Karty Lovelace</h5><button type="button" class="btn-close" data-bs-dismiss="modal"></button></div>
-  <div class="modal-body"><p>Skopiuj link i dodaj w zasobach:</p><div class="input-group mb-3"><input type="text" class="form-control bg-light" value="/local/employee-card.js" id="linkInput" readonly><button class="btn btn-outline-primary" id="btn-copy" onclick="copyLink()">Kopiuj</button></div><a href="/config/lovelace/resources" target="_blank" class="btn btn-success w-100">Otwórz Ustawienia</a></div></div></div>
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header bg-light">
+        <h5 class="modal-title">Instalacja Karty Lovelace</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+      <div class="modal-body">
+        <p>Aby użyć karty na pulpicie:</p>
+        <ol>
+            <li>Skopiuj link poniżej.</li>
+            <li>Kliknij <b>Otwórz Ustawienia</b>.</li>
+            <li>Dodaj zasób, wklej link i wybierz <b>Moduł JavaScript</b>.</li>
+        </ol>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control bg-light" value="/local/employee-card.js" id="linkInput" readonly>
+            <button class="btn btn-outline-primary" id="btn-copy" onclick="copyLink()">Kopiuj</button>
+        </div>
+        <a href="/config/lovelace/resources" target="_blank" class="btn btn-success w-100">Otwórz Ustawienia</a>
+      </div>
+    </div>
+  </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
